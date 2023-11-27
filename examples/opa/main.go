416b1aa9797e9991a4f268e4b34323487ab3587f
John Hooks <hooksie11@gmail.com>
2023-11-27 15:35:15 -0500

    Add OPA middleware

---
+++ examples/opa/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	cwhttp "github.com/CoverWhale/coverwhale-go/transports/http"
+	"github.com/CoverWhale/coverwhale-go/transports/http/middleware"
+	"github.com/CoverWhale/logr"
+)
+
+type Request struct {
+	SampleValue string `json:"sample_value"`
+}
+
+func getRoutes(l *logr.Logger) []cwhttp.Route {
+	return []cwhttp.Route{
+		{
+			Method:  http.MethodPost,
+			Path:    "/test",
+			Handler: http.HandlerFunc(test),
+		},
+		{
+			Method:  http.MethodPost,
+			Path:    "/test-custom",
+			Handler: middleware.CustomValidator(http.HandlerFunc(test), middleware.SideCarOPA, "cw/underwriting"),
+		},
+	}
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	var req Request
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		logr.Errorf("error decoding app data: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	resp := fmt.Sprintf("%s", req.SampleValue)
+
+	w.Write([]byte(resp))
+}
+
+func main() {
+	ctx := context.Background()
+
+	s := cwhttp.NewHTTPServer(
+		cwhttp.SetServerPort(7070),
+	)
+
+	s.RegisterSubRouter("/api/v1", getRoutes(s.Logger), middleware.RequestID)
+
+	errChan := make(chan error, 1)
+	go s.Serve(errChan)
+	s.AutoHandleErrors(ctx, errChan)
+}
