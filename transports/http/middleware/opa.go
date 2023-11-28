344e7122070667757499cd0b17445825e3e08a75
John Hooks <hooksie11@gmail.com>
2023-11-28 16:20:07 -0500

    add custom validation functions for OPA

--- transports/http/middleware/opa.go
+++ transports/http/middleware/opa.go
@@ -7,63 +7,27 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/CoverWhale/coverwhale-go/opa"
 	"github.com/CoverWhale/logr"
 )
 
-const (
-	SideCarOPA OPAURL = "http://localhost:8181"
-	CentralOPA OPAURL = "http://opa.svc.cluster.local:8181"
-)
-
-type OPAURL string
-
-type OPAResponse struct {
-	Result Result `json:"result"`
+type Query struct {
+	OperationName string    `json:"operationName,omitempty"`
+	Variables     Variables `json:"variables"`
 }
 
-type Result struct {
-	Allow bool     `json:"allow"`
-	Deny  []string `json:"deny,omitempty"`
+type Variables struct {
+	Data any `json:"data"`
 }
 
-type OPARequest struct {
-	Input Input `json:"input"`
-}
-
-type Input struct {
-	State        string    `json:"state"`
-	BusinessType string    `json:"business_type"`
-	Commodities  []string  `json:"commodities"`
-	Drivers      []Driver  `json:"drivers"`
-	Vehicles     []Vehicle `json:"vehicles"`
-	Trailers     []Trailer `json:"trailers"`
-}
-
-type Driver struct {
-	ID         string   `json:"id"`
-	Experience int      `json:"experience"`
-	Age        int      `json:"age"`
-	AVDs       []string `json:"avds"`
-}
-
-type Vehicle struct {
-	ID        string `json:"id"`
-	BodyType  string `json:"body_type"`
-	Class     int    `json:"class"`
-	ModelYear int    `json:"model_year"`
-	Amount    int    `json:"amount"`
-}
-
-type Trailer struct {
-	ID          string `json:"id"`
-	TrailerType string `json:"trailer_type"`
-	ModelYear   int    `json:"model_year"`
-	Amount      int    `json:"amount"`
-}
+// ValidationFunc is used to map the data in the incoming request to an OPA request.
+// It's on the caller of the service to define the data in the OPA request and
+// what package is called in OPA.
+type ValidationFunc func([]byte) (opa.OPARequest, error)
 
 // StandardValidator calls the central OPA server with the full cw validation pacakge
 func StandardValidator(h http.Handler) http.Handler {
-	url := fmt.Sprintf("%s/v1/data/cw", CentralOPA)
+	url := fmt.Sprintf("%s/v1/data/cw", opa.CentralOPA)
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
@@ -90,13 +54,12 @@ func StandardValidator(h http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(fn)
-
 }
 
 // CustomValidtor allows for calling a specific OPA instance and defining the package name
 // For example, to call a sidecar instead of the central OPA server and to only call
 // the cw/underwriting package instead of the full cw package.
-func CustomValidator(h http.Handler, url OPAURL, pkg string) http.Handler {
+func CustomValidator(h http.Handler, customValidator ValidationFunc, url opa.OPAURL, pkg string) http.Handler {
 	endpoint := fmt.Sprintf("%s/v1/data/%s", url, pkg)
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
@@ -107,12 +70,26 @@ func CustomValidator(h http.Handler, url OPAURL, pkg string) http.Handler {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
+		defer r.Body.Close()
 
 		// create two copies of request body since reading the body drains the reader
-		opaBody := io.NopCloser(bytes.NewBuffer(buf))
 		dataBody := io.NopCloser(bytes.NewBuffer(buf))
 
-		if !validate(w, opaBody, endpoint) {
+		opaRequest, err := customValidator(buf)
+		if err != nil {
+			logr.Errorf("error from custom validation func: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		requestData, err := json.Marshal(opaRequest)
+		if err != nil {
+			logr.Errorf("error marshaling input data: %v", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		if !validate(w, bytes.NewReader(requestData), endpoint) {
 			return
 		}
 
@@ -126,8 +103,79 @@ func CustomValidator(h http.Handler, url OPAURL, pkg string) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
+// GraphQLCustomValidator is like the other CustomValidator function but for GraphQL requests. It passes through data if the
+// query type is an introspection so that schema requests aren't validated by OPA
+func GraphQLCustomValidator(handler http.Handler, customValidator ValidationFunc, url opa.OPAURL, pkg string) http.Handler {
+	endpoint := fmt.Sprintf("%s/v1/data/%s", url, pkg)
+
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		buf, err := io.ReadAll(r.Body)
+		if err != nil {
+			logr.Errorf("error reading request body: %v", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		// create two copies of request body since reading the body drains the reader
+		queryBody := io.NopCloser(bytes.NewBuffer(buf))
+		dataBody := io.NopCloser(bytes.NewBuffer(buf))
+
+		data, err := io.ReadAll(queryBody)
+		if err != nil {
+			return
+		}
+		defer queryBody.Close()
+
+		var q Query
+
+		if err := json.Unmarshal(data, &q); err != nil {
+			logr.Errorf("error unmarshaling query data: %v", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		// return introspection queries directly for schema lookups.
+		if q.OperationName == "IntrospectionQuery" {
+			r.Body = dataBody
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		opaData, err := json.Marshal(q.Variables.Data)
+		if err != nil {
+			return
+		}
+
+		opaRequest, err := customValidator(opaData)
+		if err != nil {
+			logr.Errorf("error from custom validation func: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		requestData, err := json.Marshal(opaRequest)
+		if err != nil {
+			logr.Errorf("error marshaling input data: %v", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		if !validate(w, bytes.NewReader(requestData), endpoint) {
+			return
+		}
+
+		// reset the request body to the copied reader
+		r.Body = dataBody
+
+		handler.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
 func validate(w http.ResponseWriter, r io.Reader, url string) bool {
-	var or OPAResponse
+	var or opa.OPAResponse
 
 	data, err := io.ReadAll(r)
 	if err != nil {
