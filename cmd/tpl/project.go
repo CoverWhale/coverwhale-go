f197df11e1ba1a878cb475704f605b9e9015d836
John Hooks <hooksie11@gmail.com>
2024-03-26 11:51:30 -0400

    add nats context option

--- cmd/tpl/project.go
+++ cmd/tpl/project.go
@@ -691,17 +691,28 @@ func NatsHelper() []byte {
 	return []byte(`package cmd 
 
 import (
-	"github.com/nats-io/nats.go"
-	"github.com/spf13/viper"
+        "github.com/CoverWhale/logr"
+        "github.com/nats-io/jsm.go/natscontext"
+        "github.com/nats-io/nats.go"
+        "github.com/spf13/viper"
 )
 
 func newNatsConnection(name string) (*nats.Conn, error) {
-	opts := []nats.Option{nats.Name(name)}
-	if viper.GetString("credentials_file") != "" {
-		opts = append(opts, nats.UserCredentials(viper.GetString("credentials_file")))
-	}
+        opts := []nats.Option{nats.Name(name)}
 
-	return nats.Connect(viper.GetString("nats_urls"), opts...)
+        if viper.GetString("credentials_file") == "" && viper.GetString("nats_jwt") == "" {
+                logr.Debug("using NATS context")
+                return natscontext.Connect("", opts...)
+        }
+
+        if viper.GetString("nats_jwt") != "" {
+                opts = append(opts, nats.UserJWTAndSeed(viper.GetString("nats_jwt"), viper.GetString("nats_seed")))
+        }
+        if viper.GetString("credentials_file") != "" {
+                opts = append(opts, nats.UserCredentials(viper.GetString("credentials_file")))
+        }
+
+        return nats.Connect(viper.GetString("nats_urls"), opts...)
 }
 `)
 }
