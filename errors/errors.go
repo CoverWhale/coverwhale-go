b80946c1872dc1dee4829ba752f0c17d93a5e0a5
John Hooks <hooksie11@gmail.com>
2024-06-20 16:53:59 -0400

    making errors more clear

---
+++ errors/errors.go
@@ -0,0 +1,35 @@
+package errors
+
+import (
+	"fmt"
+)
+
+// ClientError represents a non-server error
+type ClientError struct {
+	Status  int
+	Details string
+}
+
+func (c ClientError) Error() string {
+	return c.Details
+}
+
+func (c *ClientError) Body() []byte {
+	return []byte(fmt.Sprintf(`{"error": "%s"}`, c.Details))
+}
+
+func (c *ClientError) Code() int {
+	return c.Status
+}
+
+func (c ClientError) As(target any) bool {
+	_, ok := target.(*ClientError)
+	return ok
+}
+
+func NewClientError(err error, code int) ClientError {
+	return ClientError{
+		Status:  code,
+		Details: err.Error(),
+	}
+}
