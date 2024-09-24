cdff55b83c8fc690590705cfe8e760f972425851
John Hooks <hooksie11@gmail.com>
2024-09-24 10:10:29 -0400

    add internal logging for client error (#96)
    * add internal logging for client error
    
    * use function options instead of new function

--- errors/errors.go
+++ errors/errors.go
@@ -6,20 +6,32 @@ import (
 
 // ClientError represents a non-server error
 type ClientError struct {
-	Status  int
+	// Status is the status code to be returned
+	Status int
+
+	// Details are a nicely formatted client error
 	Details string
+
+	//DetailedError is the actual error to be logged
+	DetailedError error
 }
+
+type ClientErrorOpt func(*ClientError)
 
 func (c ClientError) Error() string {
 	return c.Details
 }
 
 func (c ClientError) Body() []byte {
-	return []byte(fmt.Sprintf(`{"error": "%s"}`, c.Details))
+	return []byte(fmt.Sprintf(`{"error": %q}`, c.Details))
 }
 
 func (c ClientError) Code() int {
 	return c.Status
+}
+
+func (c ClientError) LoggedError() string {
+	return c.DetailedError.Error()
 }
 
 func (c ClientError) As(target any) bool {
@@ -27,9 +39,22 @@ func (c ClientError) As(target any) bool {
 	return ok
 }
 
-func NewClientError(err error, code int) ClientError {
-	return ClientError{
-		Status:  code,
-		Details: err.Error(),
+func WithDetailedError(err error) ClientErrorOpt {
+	return func(c *ClientError) {
+		c.DetailedError = err
 	}
+}
+
+func NewClientError(err error, code int, opts ...ClientErrorOpt) ClientError {
+	ce := ClientError{
+		Status:        code,
+		Details:       err.Error(),
+		DetailedError: err,
+	}
+
+	for _, v := range opts {
+		v(&ce)
+	}
+
+	return ce
 }
